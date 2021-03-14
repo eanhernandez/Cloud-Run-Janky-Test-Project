0c08206b0fcb2544d736a5501df5aa0ba83bd04f
ean hernandez <ean.hernandez@eanh.net>
2021-03-14 14:24:04 -0700

    added call to GCE over VPC plus urls

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 A sample Hello World server.
 """
 import os
-from flask import request, make_response
+import sys
 # import redis
 import urllib.request
 
+
 from flask import Flask, render_template
+from flask import request, make_response
+
+#from google.cloud import storage
 
 # pylint: disable=C0103
 app = Flask(__name__)
@@ -14,17 +18,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
+
+    timeout=5
+
     pageTitle = "Ean's Janky Cloud Run Test App"
 
-    address = "https://us-west2-www-eanh-net.cloudfunctions.net/gcf-connector"
-    gcfMessage = urllib.request.urlopen(address).read().decode('utf-8')
+    gcfaddress = "https://us-west2-saturday-307518.cloudfunctions.net/WhatIsYourFavoriteBand-1"
+    gcfMessage = urllib.request.urlopen(gcfaddress,timeout=timeout).read().decode('utf-8')
+    gceaddress = "http://10.154.0.2:8080/faveband.html"
+    try:
+        VpcMessage = urllib.request.urlopen(gceaddress,timeout=timeout).read().decode('utf-8')
+    except:
+        e = sys.exc_info()[0]
+        VpcMessage = str(e)
+
 
     queryStringMessage = request.args.get('x')
     resp = make_response(render_template('index.html',
                                          PageTitle=pageTitle,
                                          QueryStringMessage=queryStringMessage,
-                                         GCFMessage=gcfMessage))
+                                         GCFMessage=gcfMessage,
+                                         GCFaddress=gcfaddress,
+                                         VPCMessage=VpcMessage,
+                                         GCEaddress=gceaddress))
 
     if queryStringMessage:
         resp.set_cookie('testcookie', queryStringMessage)
